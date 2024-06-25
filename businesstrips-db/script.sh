bc055be4ac5ca7bbc98f448f8dfb15b33ffad0d9
Juschwy <73661431+Juschwy@users.noreply.github.com>
2024-06-25 16:31:25 +0200

    Feature/move to new org (#17)

--- businesstrips-db/script.sh
+++ businesstrips-db/script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Leider gibt es keine Konfigurationsfiles für die Postgres DB
 
-fly postgres create --autostart --name=businesstrips-db --region=ams --org=personal
+fly postgres create --autostart --name=businesstrips-db --region=ams --org=businesstrips-stack
 fly postgres attach businesstrips-db  --app=businesstrips-backend
